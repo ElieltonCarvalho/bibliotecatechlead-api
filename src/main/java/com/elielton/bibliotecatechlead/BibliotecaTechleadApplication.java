de735b693e573f975e13bceb1e53490750a359a6
ElieltonCarvalho <elieltoncarvalho2@gmail.com>
2022-04-27 11:09:48 -0300

    primeiro commit

---
+++ src/main/java/com/elielton/bibliotecatechlead/BibliotecaTechleadApplication.java
@@ -0,0 +1,13 @@
+package com.elielton.bibliotecatechlead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaTechleadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaTechleadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elielton/bibliotecatechlead/BibliotecaTechleadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elielton.bibliotecatechlead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaTechleadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
